4c560a8a130d9361ca3c7d85c2e618009ec079e1
ena <you@example.com>
2020-02-07 10:07:52 +0100

    taxe
    Signed-off-by: ena <you@example.com>

--- grb-tax-service/src/main/java/tn/gov/cni/dpp/proxy/ArticleProxy.java
+++ grb-tax-service/src/main/java/tn/gov/cni/dpp/proxy/ArticleProxy.java
@@ -1,12 +1,14 @@
 package tn.gov.cni.dpp.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import tn.gov.cni.dpp.dto.ArticleDTO;
 
-@FeignClient(name = "article-service", url = "localhost:8881")
+@FeignClient(name = "article-service")
+@RibbonClient(name = "article-service")
 public interface ArticleProxy {
 	@GetMapping("/articles/{id}")
 	ArticleDTO retrieveArticle(@PathVariable("id") Long id);
